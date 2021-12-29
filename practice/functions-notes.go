a4a07aa2d5090eb9de01d31f68e6bec315632d65
Alison Reilly <reillyamr@gmail.com>
2021-12-29 11:19:16 -0500

    added notes for functions section thru defer

--- practice/functions-notes.go
+++ practice/functions-notes.go
@@ -16,8 +16,19 @@ func main(){
 	fmt.Println(y)
 
 	//variadic parameter example cont
-	x:= foo2(2,3,4,5,6,7)
+	//can also pass in nothing i.e. foo2() or sum(), bc variadic is 0 to infinite amount
+	//would need to be the final parameter/argument as it could be no values if there were multiple params/args 
+	x := foo2(2,3,4,5,6,7)
 	fmt.Println("The total is", x)
+
+	// 'unfurl'  slice example
+	xi := []int{2,3,4,5,6,7}
+	//adds the ints above, xi..., he calls it 'unfurling' as in take all these and dump them into here
+	x := sum(xi...)
+
+	//defer example
+	defer foo3()
+	bar2()
 
 }
 
@@ -51,19 +62,40 @@ func mouse(fn, ln string) (string, bool) {
 //variadic parameter example:
 // unlimited amt of params, ie ints in this case
 //this becomes a slice of int, noted when printing out the type
+//could pass in nothing all together so is 0 to infinite amount
 func foo2(x ...int){
 	fmt.Println(x)
 	fmt.Printf("%T\n", x)
 
-	sum := 0 //preferred, more readable, alternate is var sum int
+	sumoffoo2 := 0 //preferred, more readable, alternate is var sum int
 	for i, v := range x {
-		sum += v
-		fmt.Println("for item in index position,", i, "we are now adding,", v), "to the total which is now", sum)
+		sumoffoo2 += v
+		fmt.Println("for item in index position,", i, "we are now adding,", v, "to the total which is now", sumoffoo2) 
 
 	}
-	fmt.Println("The total is,", sum)
-	return sum
+	fmt.Println("The total is,", sumoffoo2)
+	return sumoffoo2
 }
 
 //left off on unfurling a slice section
+//using similar example as foo2
+func sum(x...int) int {
+	fmt.Println(x)
+	fmt.Printf("%T\n", x)
+
+	sum := 0
+	for i, v := range x {
+		sum += v
+		fmt.Println("for item in index position", i, "we are now adding," v, "to the total which is now", sum)
+	}
+}
+
+//deferring
+func foo3(){
+	fmt.Println("foo")
+
+
+func bar2(){
+	fmt.Println("bar")
+}
 
